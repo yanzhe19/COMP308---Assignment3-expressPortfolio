eabc05a2d53a561df025e75d8636a3f76ca42e2e
yanzhe19 <yanzhe19@gmail.com>
2015-03-17 09:58:04 -0400

    added foundaation and jquery

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welocme to my portfolio' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
